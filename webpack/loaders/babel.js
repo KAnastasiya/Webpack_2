2da054822c737235676038272c48aff774ade4dc
Anastasiia <igoninan87@gmail.com>
2017-07-03 13:42:42 +0300

    Some changes

--- webpack/loaders/babel.js
+++ webpack/loaders/babel.js
@@ -1,19 +1,22 @@
 const transformObjectRestSpread = require('babel-plugin-transform-object-rest-spread');
+const test = require('babel-plugin-transform-runtime');
 
 module.exports = function () {
   return {
     module: {
       rules: [{
-        test: /\.(js)$/,
-        exclude: /(node_modules)/,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: [{
           loader: 'babel-loader',
           options: {
+            cacheDirectory: true,
             presets: [
               ['es2015', { modules: false }],
             ],
             plugins: [
               transformObjectRestSpread,
+              test,
             ],
           },
         }],
